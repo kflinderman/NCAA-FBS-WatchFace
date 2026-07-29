f5dde76f6a088957c20a98600526b27acfe603f8
kflinderman <kenny.linderman@gmail.com>
2026-07-29 11:04:11 -0400

    swap API and battery location

--- src/c/api.c
+++ src/c/api.c
@@ -428,10 +428,11 @@ void api_icon_draw(Layer *window_layer, GRect bounds){
   
   #if PBL_DISPLAY_HEIGHT > 180
     //168
-    s_api_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump - 4), bounds.size.h * vert_2 + 3, 8, 14, s_api_low_bitmap, window_layer);
+    s_api_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 19), bounds.size.h * vert_2 + 3, 8, 14, s_api_low_bitmap, window_layer);
   #else
-    s_api_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 2), bounds.size.h * vert_2 + 3, 4, 7, s_api_low_bitmap, window_layer);
+    s_api_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 14), bounds.size.h * vert_2 + 3, 4, 7, s_api_low_bitmap, window_layer);
   #endif
+  
     
   layer_set_hidden(bitmap_layer_get_layer(s_api_layer), true);
 }
--- src/c/sensors.c
+++ src/c/sensors.c
@@ -134,8 +134,8 @@ void sensor_battery_draw(Layer *window_layer, GRect bounds){
   
   #if PBL_DISPLAY_HEIGHT > 180
     //168
-    s_batt_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 20), bounds.size.h * vert_2, 8, 14, s_batt_low_bitmap, window_layer);
+    s_batt_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump - 4), bounds.size.h * vert_2, 8, 14, s_batt_low_bitmap, window_layer);
   #else
-    s_batt_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 15), bounds.size.h * vert_2 + 3, 4, 7, s_batt_low_bitmap, window_layer);
+    s_batt_layer = drawing_bitmap_set(bounds.size.w * hor_2 - (icon_bump + 2), bounds.size.h * vert_2 + 3, 4, 7, s_batt_low_bitmap, window_layer);
   #endif
 }
